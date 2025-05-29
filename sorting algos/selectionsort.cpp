16a32723e6cdd5f8740e84a1c79817e3da068c50
Shreyansh Goswami <goswami121shreyansh@gmail.com>
2025-05-29 11:43:26 +0530

    sorting

--- sorting algos/selectionsort.cpp
+++ sorting algos/selectionsort.cpp
@@ -2,19 +2,25 @@
 using namespace std;
 
 int main(){
-    int n=5;
-    int arr[n]={64,65,74,38,22};
-     for(int i=0;i<n-1;i++){
-        int min=i;
-        for(int j=i+1;j<n;j++)
-        if(arr[j]<arr[min])min=j;
-
-        int temp=arr[min];
-        arr[min]=arr[i];
-        arr[i]=temp;
+  int n ;
+  cin>> n ;
+  int arr[n];
+  for(int i=0 ; i<n ;i++){
+    cin>> arr[i];
+  }
+  for (int i=0;i<(n-1);i++){
+    int index = i;
+    for(int j=i+1;j<n;j++){ // fixed loop bounds
+      if(arr[j]<arr[index]){
+        index=j;
+      }
     }
-  for(int i=0;i< n;i++)
-printf("%d ",arr[i]);
+    swap(arr[i],arr[index]);
+  }
+
+  for(int i=0;i<n;i++){
+    cout<< arr[i]<<" ";
+  }
 
 return 0;
 }
