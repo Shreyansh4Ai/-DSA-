2022b9f43bf14a3e4e3fee6f5f6e8269c7c9ca69
Shreyansh Goswami <goswami121shreyansh@gmail.com>
2025-06-24 20:21:09 +0530

    updated

---
+++ Linked list/nodecreation.cpp
@@ -0,0 +1,26 @@
+#include<iostream> 
+using namespace std ; 
+
+class Node{
+    public:
+    int val;
+    Node*next ;
+
+    Node(value){
+          val = value ;
+          next = NULL;
+    }
+};
+
+
+int main(){
+
+Node*n = newNode(1) ; 
+cout << 
+
+
+
+
+
+    return 0 ;
+}
